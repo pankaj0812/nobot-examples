de01f36956c51caa4c9f137b23f14e5063c11ef0
pankaj0812 <singhpankajkumar65@gmail.com>
2019-02-24 22:24:34 +0530

    003 chapter done

--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,7 @@
 const querystring = require('querystring');
+const url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=28&comment=Yes+I+am+getting+old';
+const parsedUrl = querystring.parse(url.substring(url.indexOf('?')+1));
+
+console.log(`myName = ${parsedUrl.myName}`);
+console.log(`myAge = ${parsedUrl.myAge}`);
+console.log(`Oh and... ${parsedUrl.comment}`);
