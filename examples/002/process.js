1bd4bf85fa1e7f75ff13cc27282d0e9a8bcdce22
pankaj0812 <singhpankajkumar65@gmail.com>
2019-02-24 19:57:59 +0530

    002 process file completed

--- examples/002/process.js
+++ examples/002/process.js
@@ -1,7 +1,16 @@
 console.log(`The process id is ${process.pid}`);
-process.on('exit', (code) =>{
-    console.log(`The process code is ${code}`);
-});
+// process.on('exit', (code) =>{
+//     console.log(`The process code is ${code}`);
+// });
 process.stdout.write('Hello, I am writing to standard output\n');
 process.stdout.write(`Current working directory ${process.cwd()}`);
 console.log(`The process uptime is ${process.uptime()} seconds`);
+process.stdout.write('Type something and then hit enter: \n');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', () =>{
+    const chunk = process.stdin.read();
+    if(chunk!=null){
+        process.stdout.write(`this is what was typed ${chunk}`);
+        process.exit(0);
+    }
+});
