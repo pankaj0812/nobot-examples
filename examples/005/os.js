0be60a3cf8fbaf7a9d762406ad6c3dc65c37bc3a
pankaj0812 <singhpankajkumar65@gmail.com>
2019-02-25 11:10:57 +0530

    Till chapter 6 done

--- examples/005/os.js
+++ examples/005/os.js
@@ -1,1 +1,5 @@
 const os = require('os');
+const cpuCores = os.cpus();
+const coreCount=cpuCores.length;
+const coreModel=cpuCores[0].model;
+console.log(`I can see your ${coreModel} has ${coreCount} cores.`);
