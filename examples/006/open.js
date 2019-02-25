e531ab35a47089038fa82ea82dad6379f619237e
pankaj0812 <singhpankajkumar65@gmail.com>
2019-02-25 11:09:34 +0530

    Till chapter 6 done

--- examples/006/open.js
+++ examples/006/open.js
@@ -1,2 +1,21 @@
 const { platform } = require('os');
 const { exec } = require('child_process');
+const WINDOWS_PLATFORM = 'win64';
+const osPlatform = platform();
+const args = process.argv.slice(2);
+const [url] =args;
+if (url === undefined) {
+    console.error('Please enter a URL, e.g. "http://www.opencanvas.co.uk"');
+    process.exit(0);
+    }
+
+let command;
+if (osPlatform === WINDOWS_PLATFORM) {
+command = `start microsoft-edge:${url}`;
+} else {
+command = `open -a "Google Chrome" ${url}`;
+}
+console.log(`executing command: ${command}`);
+exec(command);
+// On windows machine, Edge browser opens URL
+// On mac machines, Chrome browser opens URL
